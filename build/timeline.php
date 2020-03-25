b8f45f36570ff675c651f1a41cf1a5648be6b905
Joseph Padfield <joseph.padfield@ng-london.org.uk>
2020-03-25 23:43:05 +0000

    Updating

---
+++ build/timeline.php
@@ -0,0 +1,112 @@
+<?php
+
+// visualise with: https://mermaidjs.github.io/mermaid-live-editor
+
+if (!file_exists("timeline.json"))
+	{die("ERROR: timeline.json missing\n");}
+else
+	{
+	$dets = getRemoteJsonDetails("timeline.json", false, true);
+
+	if (!isset($dets["start date"]))
+		{die("ERROR: timeline.json format problems - 'start date' not found\n");}
+		
+	$start = $dets["start date"];
+
+	$prefs = array_keys($dets["groups"]);
+	$first = $prefs[0];
+
+	if (!isset($dets["project"])) {$dets["project"] = "Please add a project title";}
+	if (!isset($dets["margin"])) {$dets["margin"] = -3;}
+		
+	array_unshift($dets["groups"][$first]["stages"],
+		array("Add as a margin", "", $dets["margin"], $dets["margin"]));
+		
+	$str = "";
+	foreach ($dets["groups"] as $pref => $ga)
+		{
+		$str .= "\tsection $ga[title]\n";
+		$no = 0;
+		foreach ($ga["stages"] as $k => $a)
+			{
+			if ($a[1]) {$a[1] = "$a[1], ";}
+			$str .= "\t\t".$a[0]." :$a[1]$pref$no, ".dA($a[2]).
+				", ".dA($a[3])."\n";
+			$no++;
+			}
+		}
+
+	//use to hide the label used for the first line which is just in place to provide a margin/padding on the left.
+	$extracss = "#".$first."0-text {display:none;}";
+
+	ob_start();
+	echo <<<END
+gantt
+       dateFormat  YYYY-MM-DD
+       title $dets[project]	
+       $str
+END;
+	$defs = ob_get_contents();
+	ob_end_clean(); // Don't send output to client	
+
+
+ob_start();
+echo <<<END
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://mermaidjs.github.io/mermaid-live-editor/src.96cd87af.css">
+  <style>
+		g a 
+			{color:inherit;}
+		$extracss
+  </style>
+</head>
+<body>
+  <div class="mermaid">
+  $defs
+  </div>
+  <script src="https://github.com/mermaid-js/mermaid/blob/develop/src/mermaid.js"></script>
+  <script>
+  mermaid.ganttConfig = {
+    titleTopMargin:25,
+    barHeight:20,
+    barGap:4,
+    topPadding:50,
+    sidePadding:50
+		}
+console.log(mermaid.render);
+  mermaid.initialize({startOnLoad:true, flowchart: { 
+    curve: 'basis' 
+  }});</script>
+</body>
+</html>
+END;
+$html = ob_get_contents();
+ob_end_clean(); // Don't send output to client	
+
+if (file_exists("../docs/timeline.html"))
+	{unlink ("../docs/timeline.html");}
+	
+$myfile = fopen("../docs/timeline.html", "w");
+fwrite($myfile, $html);
+fclose($myfile);
+echo $html;
+	
+function dA ($v)//$m, $d=0)
+	{
+	global $start;
+	$a = explode(",", $v);
+	$m = intval($a[0]);
+	if(isset($a[1]));
+		{$d = intval($a[1]);}
+	else
+		{$d = 0;}
+	$date=new DateTime($start); // date object created. 
+	$date->add(new DateInterval('P'.$m.'M'.$d.'D')); // inerval of 1 year 3 months added
+	$new = $date->format('Y-m-d'); // Output is 2020-Aug-30
+	return($new);
+	}
+	
+?>
