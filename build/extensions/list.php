d0478e10d89057a56a084a44031bee6dff9f5d25
Joseph Padfield <joseph.padfield@ng-london.org.uk>
2020-09-02 23:21:58 +0100

    Updated the new List extension

--- build/extensions/list.php
+++ build/extensions/list.php
@@ -1,29 +1,61 @@
 <?php
 
 $extensionList["list"] = "extensionCards";
-
+$blank = array("groups" => array(), "ptitle" => "",
+		"stitle" => "",  "comment" => "", "image" => "", "link" => "");
+		
 // Still to do an optional table of contents for groups - model on https://github.com/IIIF/awesome-iiif
 
+function buildContents ($groups)
+	{
+	$html = "<ul>";
+
+	foreach ($groups as $gnm => $ga)
+		{$tag = urlencode(strtolower($gnm));
+ 		 $html .= "<li>".
+			"<a id=\"$tag-TBC\" class=\"offsetanchor\"></a>".
+			"<a href=\"#$tag\">$gnm</a></li>";}
+	
+	$html .= "</ul>";
+
+	return ($html);
+	}
+
+/*
+
+<svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg>
+* 
+ <h2><a id="user-content-standards" class="anchor" aria-hidden="true" href="#standards"></a>Standards</h2>
+ 
+ */
+	
 function extensionCards ($d, $pd)
   {
+	global $blank;
   $gcontent = "";
 		
 	if (isset($d["file"]) and file_exists($d["file"]))
 		{
 		$dets = getRemoteJsonDetails($d["file"], false, true);
 
+		// Check if a table of contents should be added.
+		if (!isset($dets["tableofcontents"])) {$dets["tableofcontents"] = false;}
+		if ($dets["tableofcontents"])
+			{$tb = buildContents ($dets["groups"]);}
+		else
+			{$tb = "";}
+
 		foreach ($dets["list"] as $lno => $la)
 			{
 			//ensure each of the currently required fields a re present.
-			$la = array_merge(
-					array("groups" => array(), "ptitle" => "", "stitle" => "",  "comment" => "",
-						"image" => "", "link" => ""), $la);
+			$la = array_merge($blank, $la);
 
 			// Testing
 			$la["comment"] = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 			foreach ($la["groups"] as $k => $gnm)
-					{					
+					{
+								
 					if (!isset($dets["groups"] [$gnm] ["card"]))
 						{$dets["groups"] [$gnm] ["card"] = "simple";}
 
@@ -34,16 +66,27 @@ function extensionCards ($d, $pd)
 					
 					if (!isset($dets["groups"] [$gnm] ["html"]))
 						{
-						$gtop = "<h3>$gnm</h3><p>".$dets["groups"] [$gnm] ["comment"]."</p>";
+						$tag = urlencode(strtolower($gnm));
+
+						if ($dets["tableofcontents"])
+							{$anchor = "<a id=\"$tag\" class=\"anchor offsetanchor\" ".
+								"aria-hidden=\"true\" href=\"#${tag}-TBC\"></a>";
+							 $alink = "<a class=\"anchor nodec\" aria-hidden=\"true\" ".
+								"href=\"#${tag}-TBC\">$gnm</a>";}
+						else
+							{$anchor = "";
+							 $alink = "$gnm";}
+						
+						$gtop = "<h3>$anchor$alink</h3><p>".$dets["groups"] [$gnm] ["comment"]."</p>";
 						if (in_array($dets["groups"] [$gnm] ["card"], array("image")))
 							{$dets["groups"] [$gnm] ["html"] = "$gtop<div class=\"row row-cols-1 ".
-								"row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5\">";}
-						else if (in_array($dets["groups"] [$gnm] ["card"], array("simple")))
-							{$dets["groups"] [$gnm] ["html"] = "$gtop<div class=\"card-deck\">";}
+								"row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5\">";}						
 						else if (in_array($dets["groups"] [$gnm] ["card"], array("list")))
 							{$dets["groups"] [$gnm] ["html"] = "$gtop<ul>";}							
-						else
+						else if (in_array($dets["groups"] [$gnm] ["card"], array("full")))
 							{$dets["groups"] [$gnm] ["html"] = "$gtop<div class=\"card-column\">";}
+						else //if (in_array($dets["groups"] [$gnm] ["card"], array("simple"))) or anything else
+							{$dets["groups"] [$gnm] ["html"] = "$gtop<div class=\"card-deck\">";}
 						}
 						
 					$dets["groups"] [$gnm] ["html"] .= call_user_func_array($cfn, array($la));					
@@ -87,9 +130,15 @@ function extensionCards ($d, $pd)
 .card-hov:hover {
 	opacity: 0.7;
 }
+
+.offsetanchor {
+	position: relative;
+  top: -75px;
+}
+
 ";
 
-    $d["content"] = positionExtraContent ($d["content"], $gcontent);
+    $d["content"] = positionExtraContent ($d["content"], $tb.$gcontent);
 		}
 
   return (array("d" => $d, "pd" => $pd));
@@ -117,7 +166,6 @@ function extensionCards ($d, $pd)
         <h4 class="card-title">$la[ptitle]</h4>
         <h5 class="card-title">$la[stitle]</h5>
         <p class="card-text">$la[comment]</p>
-        <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
       </div>
     </div>
   </div>$lbottom
